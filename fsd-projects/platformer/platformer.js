e3d70b22a4563274d04790ced295a6c0d8397853
FaTaL-01 <blake.theriot@jpschools.org>
2026-09-03 15:41:10 +0000

    Update platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -12,42 +12,57 @@ $(function () {
       $(document).on("keydown", handleKeyDown);
       $(document).on("keyup", handleKeyUp);
       firstTimeSetup = false;
+      runStartedAt = performance.now();
       //start game
       setInterval(main, 1000 / frameRate);
     }
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(
+      -50,
+      canvas.height - 10,
+      canvas.width + 100,
+      200,
+      "rgb(118, 0, 233)",
+    ); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
-    //////////////////////////////////
-    // ONLY CHANGE BELOW THIS POINT //
-    //////////////////////////////////
+    // ONL BELOW POINT //
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    //toggleGrid();
 
     // TODO 2 - Create Platforms
 
+    createPlatform(300, 607, 150, 10, "blue");
 
+    createPlatform(600, 500, 150, 10, "green");
 
+    createPlatform(900, 400, 150, 10, "purple");
+
+    createPlatform(600, 300, 150, 10, "green");
+
+    createPlatform(300, 200, 150, 10, "blue");
 
     // TODO 3 - Create Collectables
 
+    createCollectable("max", 300, 550, 0, 0);
 
+    createCollectable("diamond", 1000, 350, 0, 0);
 
-    
+    createCollectable("steve", 300, 150, 0, 0);
+
     // TODO 4 - Create Cannons
 
+    createCannon("top", 450, 900);
 
-    
-    
-    //////////////////////////////////
-    // ONLY CHANGE ABOVE THIS POINT //
-    //////////////////////////////////
+    createCannon("right", 750, 3500);
+
+    createCannon("left", 200, 2000);
+
+    // ONLY  POINT //
   }
 
   registerSetup(setup);
